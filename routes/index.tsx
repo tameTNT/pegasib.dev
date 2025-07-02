d1fcb0074a9001e67c8962722d6129776c3dcf7e
Luca <30503695+tameTNT@users.noreply.github.com>
2025-07-02 21:06:14 +0100

    🎉 Add front page with space parallax effect

---
+++ routes/index.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+      <div className="text-white" id="scene">
+        <script type="text/javascript" src="/background_parallax.js" defer></script>
+        <div className="flex flex-row min-h-screen justify-center items-center">
+          <p className="font-bold">51 Pegasi B</p>
+        </div>
+        <div className="absolute right-1 bottom-1">
+          <i>By ESO/Digitized Sky Survey 2 - <a rel="nofollow" class="external text" target="_blank"
+                                                href="https://www.eso.org/public/images/eso1517c/">ESO
+            website</a>, <a href="https://creativecommons.org/licenses/by/4.0" target="_blank"
+                            title="Creative Commons Attribution 4.0">CC
+            BY 4.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=39719449"
+                           target="_blank">Link</a></i>
+        </div>
+      </div>
+  );
+}
