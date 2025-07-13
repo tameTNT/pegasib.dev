ee0d600967f4cb4302d1184ba2814b7ece14d769
Luca <30503695+tameTNT@users.noreply.github.com>
2025-07-13 13:51:19 +0100

    ✨ Resize link icons when cursor is nearby

--- static/cursor_effects.js
+++ static/cursor_effects.js
@@ -1,6 +1,7 @@
 // const container = document.getElementById("container");
 const scene = document.getElementById("scene");
 const title = document.getElementById("title");
+const icons = document.getElementsByClassName("icon");
 const root = document.documentElement;
 
 scene.classList.add("resting");
@@ -10,12 +11,16 @@ setTimeout(() => {
   title.classList.remove("resting");
 }, 1000)
 
+function convertToRelative(x, y, width, height) {
+  const relativeX = (x - width / 2) / width;
+  const relativeY = (y - height / 2) / height;
+  return [relativeX, relativeY]
+}
+
 document.addEventListener("mousemove", (e) => {
   const window_width = document.documentElement.clientWidth;
   const window_height = document.documentElement.clientHeight;
-  const relativeX = (e.pageX - window_width / 2) / window_width;
-  const relativeY = (e.pageY - window_height / 2) / window_height;
-
+  const [relativeX, relativeY] = convertToRelative(e.pageX, e.pageY, window_width, window_height)
   const angleRad = Math.atan2(relativeY, relativeX);
 
   // Convert radians to degrees.
@@ -34,6 +39,23 @@ document.addEventListener("mousemove", (e) => {
   root.style.setProperty("--xPos", `${50 + xOffset}%`);
   root.style.setProperty("--yPos", `${50 + yOffset}%`);
   root.style.setProperty("--gradientAngle", `${angleDeg}deg`);
+
+  for (let icon of icons) {
+    const bounds = icon.getBoundingClientRect();
+    let midX = bounds.right - (bounds.right - bounds.left) / 2;
+    let midY = bounds.bottom - (bounds.bottom - bounds.top) / 2;
+    [midX, midY] = convertToRelative(midX, midY, window_width, window_height)
+    // console.log(midX, midY);
+
+    const prox = 0.15;  // todo: make this relative to offset between icons
+    const xtoCursor = Math.abs(midX - relativeX);
+    const ytoCursor = Math.abs(midY - relativeY);
+    if (xtoCursor < prox && ytoCursor < prox) {
+      icon.style = "scale: 1.5";
+    } else {
+      icon.style = "scale: 1";
+    }
+  }
 });
 
 document.addEventListener("mouseleave", () => {
