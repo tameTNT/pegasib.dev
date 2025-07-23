1f7d08b16d8639f1ba5bcbb6fbe29f65d8d34f1c
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-07-23 23:21:38 +0100

    🔨 Add reboot actions python script to VCS

---
+++ ec2_reboot_actions.py
@@ -0,0 +1,66 @@
+# /// script
+# dependencies = [
+#   "cloudflare",
+#   "python-dotenv",
+#   "PyGithub",
+# ]
+# ///
+
+from datetime import datetime, timezone
+import os
+import urllib.request
+
+from cloudflare import Cloudflare
+from dotenv import load_dotenv
+from github import Github, Auth
+
+
+load_dotenv()  # Loads CLOUDFLARE_API_TOKEN and CLOUDFLARE_ZONE_ID as well as GITHUB_ACCESS_TOKEN and GITHUB_REPO_URL
+
+external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+print(f"EC2 server's new public IPv4 address: {external_ip}")
+
+
+# == Update DNS record with server's new IPv4 address ==
+client = Cloudflare()
+zone_id = os.getenv("CLOUDFLARE_ZONE_ID")
+
+domain_name = "pegasib.dev"
+subdomains_to_update = ["51", "loona-heardle"]
+for subdomain in subdomains_to_update:
+    page = client.dns.records.list(
+        zone_id=zone_id,
+        type="A",
+        name={"startswith": subdomain},
+    )
+
+    if page.success:
+        result = page.result[0]
+        target_record_id = result.id
+
+        record_edit_resp = client.dns.records.edit(
+            dns_record_id=target_record_id,
+            zone_id=zone_id,
+            name=f"{subdomain}.{domain_name}",
+            ttl=1,
+            type="A",
+            comment=f"Updated via API at {datetime.now(timezone.utc).isoformat()}",
+            content=external_ip,
+            proxied=True,
+        )
+        print("Successfully updated Cloudflare DNS record with new IP address.")
+    else:
+        raise Exception("Unable to locate correct DNS record")
+
+# == Update GitHub repo actions REMOTE_HOST secret with new IPv4 address (for GitHub actions) ==
+github_auth = Auth.Token(os.getenv("GITHUB_ACCESS_TOKEN"))
+g = Github(auth=github_auth)
+
+target_repo = g.get_repo(os.getenv("GITHUB_REPO_URL"))
+target_repo.create_secret(
+    secret_name="REMOTE_HOST",
+    unencrypted_value=external_ip,
+    secret_type="actions",
+)
+g.close()
+print("Successfully updated GitHub repo actions secret REMOTE_HOST with new IP address.")
