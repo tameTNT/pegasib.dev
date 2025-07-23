b46d3bc6b8dbf29aa876f5aa89a6e5d8491dcfde
Luca <30503695+tameTNT@users.noreply.github.com>
2025-07-23 16:03:51 +0100

    🏷️ Use Typescript for client side files
    Requires running `deno bundle` command on changes to compile ts->js :(

--- deno_server/static/bg_cursor_move.ts
+++ deno_server/static/bg_cursor_move.ts
@@ -1,52 +1,71 @@
-// bg_cursor_move.ts
-var scene = document.getElementById("spaceScene");
-var title = document.querySelector("h1");
-var root = document.documentElement;
-function convertToRelative(x, y, width, height) {
+const scene = document.getElementById("spaceScene");
+const title = document.querySelector("h1");
+const root = document.documentElement;
+
+function convertToRelative(
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+) {
   const relativeX = (x - width / 2) / width;
   const relativeY = (y - height / 2) / height;
-  return [
-    relativeX,
-    relativeY
-  ];
+  return [relativeX, relativeY];
 }
+
 if (scene !== null && title !== null) {
   scene.classList.add("resting");
   title.classList.add("resting");
   setTimeout(() => {
     scene.classList.remove("resting");
     title.classList.remove("resting");
-  }, 1e3);
+  }, 1000);
+
   document.addEventListener("mousemove", (e) => {
     const window_width = document.documentElement.clientWidth;
     const window_height = document.documentElement.clientHeight;
-    const [relativeX, relativeY] = convertToRelative(e.pageX, e.pageY, window_width, window_height);
+    const [relativeX, relativeY] = convertToRelative(
+      e.pageX,
+      e.pageY,
+      window_width,
+      window_height,
+    );
     const angleRad = Math.atan2(relativeY, relativeX);
+
+    // Convert radians to degrees.
     let angleDeg = Math.trunc(angleRad * (180 / Math.PI) + 90);
     if (angleDeg < 0) angleDeg += 360;
+
     const debugEl = document.getElementById("debug");
     if (debugEl !== null) {
-      debugEl.innerText = `x=${relativeX.toFixed(3)},y=${relativeY.toFixed(3)},angle=${angleDeg}`;
+      debugEl.innerText = `x=${relativeX.toFixed(3)},y=${
+        relativeY.toFixed(3)
+      },angle=${angleDeg}`;
     }
-    const offsetSeverity = 0.01;
-    const ratioAdjustment = 3 * window_width / window_height;
+
+    const offsetSeverity = 0.01; // higher number for more background movement (0.01)
+    const ratioAdjustment = 3 * window_width / window_height; // tune width/height movement (3)
     const xOffset = relativeX * ratioAdjustment * window_width * offsetSeverity;
     const yOffset = relativeY * window_height * offsetSeverity;
+
     root.style.setProperty("--xPos", `${50 + xOffset}%`);
     root.style.setProperty("--yPos", `${50 + yOffset}%`);
     root.style.setProperty("--gradientAngle", `${angleDeg}deg`);
   });
+
   document.addEventListener("mouseleave", () => {
     root.style.setProperty("--xPos", "50%");
     root.style.setProperty("--yPos", "50%");
     root.style.setProperty("--gradientAngle", "90deg");
+
     scene.classList.add("resting");
     title.classList.add("resting");
   });
+
   document.addEventListener("mouseenter", () => {
     setTimeout(() => {
       scene.classList.remove("resting");
       title.classList.remove("resting");
-    }, 1e3);
+    }, 1000); // todo: fix snapping to new position after 'resting' ends
   });
 }
--- deno_server/static/home_icon_swell.ts
+++ deno_server/static/home_icon_swell.ts
@@ -1,15 +1,18 @@
-// home_icon_swell.ts
-var icons = document.querySelectorAll("div.icon");
+const icons: NodeListOf<HTMLDivElement> = document.querySelectorAll("div.icon");
+
 document.addEventListener("mousemove", (e) => {
   for (const icon of icons) {
     const bounds = icon.getBoundingClientRect();
     const midX = bounds.right - (bounds.right - bounds.left) / 2;
     const midY = bounds.bottom - (bounds.bottom - bounds.top) / 2;
-    const maxScale = 1.5;
-    const scaleFactor = 200;
+
+    const maxScale = 1.5; // sets maximum size icons grow to (1.5)
+    const scaleFactor = 200; // higher number activates icons from further away (200)
     const xtoCursor = Math.abs(midX - e.pageX);
     const ytoCursor = Math.abs(midY - e.pageY);
     const distance = Math.sqrt(xtoCursor ** 2 + ytoCursor ** 2);
-    icon.style.scale = Math.max(maxScale - distance / scaleFactor, 1).toFixed(3);
+    icon.style.scale = Math.max(maxScale - distance / scaleFactor, 1).toFixed(
+      3,
+    );
   }
 });
