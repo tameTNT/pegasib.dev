7cdff770a2a847037ad6001cb6ec3356ae03bffd
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-07-25 22:24:12 +0100

    💬 Clean up todays-song.ts comments and logic
    Separate out constants

--- heardle_server/routes/api/todays-song.ts
+++ heardle_server/routes/api/todays-song.ts
@@ -3,13 +3,13 @@ import { randomSeeded, shuffle } from "@std/random";
 import * as pathTools from "node:path";
 import * as os from "node:os";
 
-
-const MILLISENDS_IN_DAY = 24 * 60 * 60 * 1000;
+const MS_PER_DAY = 24 * 60 * 60 * 1000;
+const START_DATE = new Date(Date.UTC(2025, 7, 25)); // when adding a new song, step back accordingly to account for it
 
 function loadSongData(): Array<object> {
   const decoder = new TextDecoder("utf-8");
 
-  const dataPath = pathTools.resolve(os.homedir(), "loona_track_info.json")
+  const dataPath = pathTools.resolve(os.homedir(), "loona_track_info.json");
   const rawData = Deno.readFileSync(dataPath);
   const jsonData = decoder.decode(rawData);
 
@@ -17,22 +17,19 @@ function loadSongData(): Array<object> {
 }
 
 function randomDateIndex(max: number) {
-  // for(let i = 0; i < 2*max; i++) {
-  const startDate = new Date(Date.UTC(2025, 7, 25));
-  const todayDate = new Date();
-  // const todayDate = new Date(Date.UTC(2025, 7, 25) + i*MILLISENDS_IN_DAY);
-  const daysDiff = Math.ceil(Math.abs(todayDate.getTime() - startDate.getTime()) / MILLISENDS_IN_DAY);
+  const todaysDate = new Date();
 
-  const indices = Array.from({length: max}, (_, i) => i);
-  const shuffledIndices = shuffle(indices, {prng: randomSeeded(42n)});
-    // console.log(indices[daysDiff % max]);
-  // }
+  const daysDiff = Math.ceil(
+    Math.abs(todaysDate.getTime() - START_DATE.getTime()) / MS_PER_DAY,
+  );
 
-  return shuffledIndices[daysDiff % max];  // todo: only works if the underlying array remains the same length...
+  const indices = Array.from({ length: max }, (_, i) => i);
+  const shuffledIndices = shuffle(indices, { prng: randomSeeded(42n) });
+
+  return shuffledIndices[daysDiff % max]; // todo: only works if the underlying array remains the same length...
 }
 
 export const handler = (_req: Request, _ctx: FreshContext): Response => {
-  // const randomIndex = Math.floor(Math.random() * JOKES.length);
   const body = loadSongData();
   const selectedIndex = randomDateIndex(body.length);
   return new Response(JSON.stringify(body[selectedIndex]));
