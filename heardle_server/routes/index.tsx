f204831e7e6cb826451b7c370befb333ae42dd5f
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-08-05 17:58:33 +0100

    Add GitHub source code link to page

--- heardle_server/routes/index.tsx
+++ heardle_server/routes/index.tsx
@@ -13,17 +13,22 @@ export default function Home() {
 
 
   return (
-    <div class="mx-auto flex flex-col h-screen justify-between items-center">
-      <main class="text-center">
-        <h1 className="text-4xl">LOOΠ∆ Heardle</h1>
-        <p class="">Includes solo, subunit, and all post-BBC tracks.</p>
-        <p class="italic text-xs">All audio courtesy of Spotify.</p>
-        <ProgressBlock allowed={ALLOWED_GUESSES} count={guessCount} history={guessHistory} />
-      </main>
-      <footer class="sticky bottom-0 w-full bg-gray-500/40 flex flex-col items-center p-2 gap-1">
-        <SongBar allowed={ALLOWED_GUESSES} count={guessCount} history={guessHistory} />
-        <GuessBar allowed={ALLOWED_GUESSES} count={guessCount} history={guessHistory} />
-      </footer>
-    </div>
+    <>
+      <a href="https://github.com/tameTNT/pegasib.dev/tree/main/heardle_server" target="_blank" class="cursor-pointer">
+        <img src="github_icon.svg" alt="The GitHub Icon" class="absolute top-1 left-1 h-5"/>
+      </a>
+      <div class="mx-auto flex flex-col h-screen justify-between items-center">
+        <main class="text-center">
+          <h1 class="text-4xl">LOOΠ∆ Heardle</h1>
+          <p class="">Includes solo, subunit, and all post-BBC tracks.</p>
+          <p class="italic text-xs">All audio courtesy of Spotify.</p>
+          <ProgressBlock allowed={ALLOWED_GUESSES} count={guessCount} history={guessHistory}/>
+        </main>
+        <footer class="sticky bottom-0 w-full bg-gray-500/40 flex flex-col items-center p-2 gap-1">
+          <SongBar allowed={ALLOWED_GUESSES} count={guessCount} history={guessHistory}/>
+          <GuessBar allowed={ALLOWED_GUESSES} count={guessCount} history={guessHistory}/>
+        </footer>
+      </div>
+    </>
   );
 }
