d19d26c5601997f8b534da8aaa7dca43f5e758d8
tameTNT <30503695+tameTNT@users.noreply.github.com>
2025-08-08 23:00:04 +0100

    🖼️ Add explicit favicon instead of PNG
    Allows API routes to use the favicon too

--- heardle_server/routes/_app.tsx
+++ heardle_server/routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/png" href="choerry_vibe.png" />
+        <link rel="icon" type="image/png" href="favicon.ico" />
         <title>LOONA Heardle</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
