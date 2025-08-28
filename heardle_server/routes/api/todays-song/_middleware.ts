3decdaf2994b6c1d5939d2fb6c3b98f6113596f9
Luca <30503695+tameTNT@users.noreply.github.com>
2025-08-28 15:24:15 +0200

    🚸 Avoid cycle in songs for longer by adding repetitions of array

--- heardle_server/routes/api/todays-song/_middleware.ts
+++ heardle_server/routes/api/todays-song/_middleware.ts
@@ -3,6 +3,7 @@ import { randomSeeded, shuffle } from "@std/random";
 
 const MS_PER_DAY = 24 * 60 * 60 * 1000;
 const START_DATE = new Date(Date.UTC(2025, 6, 25)); // when adding a new song, step back accordingly to account for it
+const REPETITIONS = 20;  // cycle won't repeat for approx 10 years if array is 200 items long
 
 function randomDateIndex(max: number) {
   const todaysDate = new Date(); // getTime is in UTC, so no need to convert
@@ -11,7 +12,7 @@ function randomDateIndex(max: number) {
     Math.abs(todaysDate.getTime() - START_DATE.getTime()) / MS_PER_DAY,
   );
 
-  const indices = Array.from({ length: max }, (_, i) => i);
+  const indices = Array.from({ length: max * REPETITIONS}, (_, i) => i % max);
   const shuffledIndices = shuffle(indices, { prng: randomSeeded(42n) });
 
   return shuffledIndices[daysDiff % max]; // todo: only works if the underlying array remains the same length...
