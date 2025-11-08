d3a5f5ed5df18df6e88cb462bb80c9ce12f00ef7
Luca <30503695+tameTNT@users.noreply.github.com>
2025-11-08 19:20:45 +0000

    🌐 Update nginx redirect from old url

--- heardle_server/routes/_app.tsx
+++ heardle_server/routes/_app.tsx
@@ -7,19 +7,19 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/png" href="favicon.ico" />
-        <title>LOONA Heardle</title>
+        <title>Kpop Heardle</title>
         <link rel="stylesheet" href="/styles.css" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://loona-heardle.pegasib.dev" />
+        <meta property="og:url" content="https://kpop-heardle.pegasib.dev" />
         <meta property="og:title" content="🎧 LOONA Heardle 🌌" />
         <meta
           property="og:description"
-          content="Guess the LOONA song (including all solo, subunit, and post-BBC songs – up to Soft Error) from as short an audio clip as possible. New song every day."
+          content="Guess the LOONA song (including all solo, subunit, and post-BBC songs – up to Soft Error) from as short an audio clip as possible. New song every day. GFriend songs now also playable!"
         />
         <meta
           property="og:image"
-          content="https://loona-heardle.pegasib.dev/preview_poster.png"
+          content="https://kpop-heardle.pegasib.dev/preview_poster.png"
         />
         {/*<meta name="twitter:card" content="summary_large_image"/>*/}
       </head>
