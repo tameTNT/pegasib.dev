188c0e10ec046e1d7cbaad0e82f316847994dc96
Luca <30503695+tameTNT@users.noreply.github.com>
2025-11-08 16:48:00 +0000

    ✨ Update Spotipy script to be able to fetch data for other artists

--- heardle_server/update_track_info.py
+++ heardle_server/update_track_info.py
@@ -7,6 +7,7 @@
 
 # Run via `uv run heardle_server/update_track_info.py`
 
+import argparse
 import json
 import os
 from pathlib import Path
@@ -20,13 +21,6 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 from dotenv import load_dotenv
 
-load_dotenv()  # Loads SPOTIFY_CLIENT_ID, and SPOTIFY_CLIENT_SECRET
-
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
-    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
-    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
-))
-
 
 def extract_track_from_playlist_item(item: dict):
     track_item = item["track"]
@@ -37,37 +31,57 @@ def extract_track_from_playlist_item(item: dict):
     return track_item
 
 
-# Get tracks from playlist at https://open.spotify.com/playlist/05bRCDfqjNVnysz17hocZn
-print("Loading tracks from https://open.spotify.com/playlist/05bRCDfqjNVnysz17hocZn")
-PLAYLIST_URI = "05bRCDfqjNVnysz17hocZn"
-INITIAL_OFFSET = 0
-tracks_got = []
-playlist_resp = sp.playlist_items(PLAYLIST_URI, offset=INITIAL_OFFSET)
-tracks_got += map(extract_track_from_playlist_item, playlist_resp["items"])
-while len(tracks_got) + INITIAL_OFFSET < playlist_resp["total"]:
-    playlist_resp = sp.playlist_items(PLAYLIST_URI, offset=len(tracks_got)+INITIAL_OFFSET)
+def download_playlist(uri: str, output_file_name: str, initial_offset: int = 0, sp_obj: spotipy.Spotify = None):
+    print(f"Loading tracks from https://open.spotify.com/playlist/{uri}")
+    tracks_got = []
+    playlist_resp = sp.playlist_items(uri, offset=initial_offset)
     tracks_got += map(extract_track_from_playlist_item, playlist_resp["items"])
+    while len(tracks_got) + initial_offset < playlist_resp["total"]:
+        playlist_resp = sp.playlist_items(uri, offset=len(tracks_got)+initial_offset)
+        tracks_got += map(extract_track_from_playlist_item, playlist_resp["items"])
 
-print(f"Got {len(tracks_got)} tracks from playlist successfully (INITIAL_OFFSET={INITIAL_OFFSET}).")
+    print(f"Got {len(tracks_got)} tracks from playlist successfully (initial_offset={initial_offset}).")
 
-# Scrape the mp3 preview URLs manually
-full_track_info = []
-for i, track in enumerate(tracks_got):
-    print(f"On track: {i+1: 3}/{len(tracks_got):03}", end="\r")
-    track_resp = requests.get(track["url"])
-    if track_resp.status_code == 200:
-        raw = track_resp.content.decode("utf-8")
-        preview_re_search = re.search(r'content="(https://p\.scdn\.co/mp3-preview/.+?)"/>', raw)
-        if preview_re_search:
-            track["preview_url"] = preview_re_search.group(1)
-            full_track_info.append(track)
+    # Scrape the mp3 preview URLs manually
+    full_track_info = []
+    for i, track in enumerate(tracks_got):
+        print(f"On track: {i+1: 3}/{len(tracks_got):03}", end="\r")
+        track_resp = requests.get(track["url"])
+        if track_resp.status_code == 200:
+            raw = track_resp.content.decode("utf-8")
+            preview_re_search = re.search(r'content="(https://p\.scdn\.co/mp3-preview/.+?)"/>', raw)
+            if preview_re_search:
+                track["preview_url"] = preview_re_search.group(1)
+                full_track_info.append(track)
+            else:
+                print(f"Failed to find preview URL for {track['name']}\n")
         else:
-            print(f"Failed to find preview URL for {track['name']}\n")
-    else:
-        print(f"Got non-OK {track_resp.status_code} status for {track['name']}\n")
+            print(f"Got non-OK {track_resp.status_code} status for {track['name']}\n")
 
-    time.sleep(random.random() / 2)  # to avoid spamming the page/avoid rate limiting
+        time.sleep(random.random() / 2)  # to avoid spamming the page/avoid rate limiting
 
-output_file = Path("~/heardle_track_info.json").expanduser()
-json.dump(full_track_info, output_file.open("w+"), indent=2)
-print(f"Done. Dumped info of {len(full_track_info)} songs to to {output_file}.")
+    output_file = Path(f"~/{output_file_name}").expanduser()
+    json.dump(full_track_info, output_file.open("w+"), indent=2)
+    print(f"Done. Dumped info of {len(full_track_info)} songs to to {output_file}.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # Defaults to LOONA songs playlist uri
+    parser.add_argument("-u", "--uri", default="05bRCDfqjNVnysz17hocZn")
+    parser.add_argument("-f", "--output-file", default="heardle_track_info.json")
+    parser.add_argument("-o", "--offset", default=0)
+    args = parser.parse_args()
+
+    uri = args.uri
+    cli_output_file = args.output_file
+    offset = args.offset
+
+    load_dotenv()  # Loads SPOTIFY_CLIENT_ID, and SPOTIFY_CLIENT_SECRET
+
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+        client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+        client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+    ))
+
+    download_playlist(uri, cli_output_file, offset, sp)
